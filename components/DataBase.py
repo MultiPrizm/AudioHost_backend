c5054499c8019cc61b0e18250a6615b23c8470cc
MultyPrism <76118715+darkdragon228@users.noreply.github.com>
2024-08-02 18:31:51 +0300

    0.5

--- components/DataBase.py
+++ components/DataBase.py
@@ -1,4 +1,4 @@
-import dotenv, os, hashlib, colorama, components.logger, sys
+import dotenv, os, hashlib, colorama, components.logger, sys, json
 
 import mysql
 import mysql.connector
@@ -31,7 +31,7 @@ class MSDB():
         if 0 != len(res):
             return False
 
-        self.cursor.execute("INSERT INTO users (login, password, usertype) VALUES (%s, %s, %s)", (login, hashlib.sha256(password.encode()).hexdigest(), "user"))
+        self.cursor.execute("INSERT INTO users (login, password, usertype, playlist) VALUES (%s, %s, %s, %s)", (login, hashlib.sha256(password.encode()).hexdigest(), "user", json.dumps({"1": []})))
         self.db.commit()
         return True
     
@@ -63,12 +63,60 @@ class MSDB():
             
             for i in songs_id_list:
 
-                self.cursor.execute("SELECT download_endpoint FROM audiolist WHERE id = %s", (int(i),))
+                self.cursor.execute("SELECT * FROM audiolist WHERE id = %s", (int(i),))
                 songs_list.append(self.cursor.fetchall())
             
             return songs_list
         except Exception as e:
             components.logger.error(e)
+
             return []
+        
+    def get_playlist(self, login):
+
+        try:
+            self.cursor.execute("SELECT playlist FROM users WHERE login = %s", (login,))
+            res = self.cursor.fetchall()
+            res = json.loads(res[0][0])
+            return res["1"]
+
+        except Exception as e:
+            components.logger.error(e)
+
+            return []
+    
+    def update_playlist(self, _list: list, login: str) -> bool:
+        try:
+            # Перевірка типу _list
+            if not isinstance(_list, list):
+                raise TypeError("Expected _list to be of type list")
+
+            # Перетворення списку в JSON-рядок
+            playlist = {"1": _list}
+            playlist_json = json.dumps(playlist)  # Це має бути рядок JSON
+
+            # Виконання запиту
+            self.cursor.execute(
+                "UPDATE users SET playlist = %s WHERE login = %s",
+                (playlist_json, login)
+            )
+            self.db.commit()
+
+            return True
+
+        except Exception as e:
+            components.logger.error("Error updating playlist: %s", e)
+            return False
+    
+    def get_tracks_from_id(self, idlist: list):
+
+        tracklist = []
+
+        for i in idlist:
+
+            self.cursor.execute("SELECT * FROM audiolist WHERE id = %s", (int(i),))
+            tracklist.append(self.cursor.fetchall())
+        
+        return tracklist
 
     
--- components/InfoCenter.py
+++ components/InfoCenter.py
@@ -12,3 +12,48 @@ async def get_audio_list(page: int):
     }
 
     return fastapi.responses.JSONResponse(res)
+
+@app.get("/info/playlist")
+async def get_audio_list(user: fastapi.Request):
+
+    audiolist = db.get_playlist(user.cookies.get("login"))
+
+    audiolist = db.get_tracks_from_id(audiolist)
+    
+    res = {
+        "response": audiolist
+    }
+
+    return fastapi.responses.JSONResponse(res)
+
+@app.post("/info/playlist/add/{id}")
+async def get_audio_list(user: fastapi.Request, id: str):
+
+    audiolist = db.get_playlist(user.cookies.get("login"))
+
+    if not id in audiolist:
+        audiolist.append(id)
+
+    dbres = db.update_playlist(audiolist, user.cookies.get("login"))
+    
+    res = {
+        "success": dbres
+    }
+
+    return fastapi.responses.JSONResponse(res)
+
+@app.post("/info/playlist/del/{id}")
+async def get_audio_list(user: fastapi.Request, id: str):
+
+    audiolist = db.get_playlist(user.cookies.get("login"))
+
+    if id in audiolist:
+        audiolist.remove(id)
+    print(audiolist)
+    dbres = db.update_playlist(audiolist, user.cookies.get("login"))
+    
+    res = {
+        "success": dbres
+    }
+
+    return fastapi.responses.JSONResponse(res)
